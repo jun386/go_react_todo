ab96d5c13ea35bb21e5c7e5a553a3ce5f7b5db3b
jun386 <aka6116.aka@gmail.com>
2023-11-24 18:07:10 +0900

    backend作成

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"backend/controller"
+	"backend/db"
+	"backend/repository"
+	"backend/router"
+	"backend/usecase"
+	"backend/validator"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	db := db.NewDB()
+	taskValidator := validator.NewTaskValidator()
+	taskRepository := repository.NewTaskRepository(db)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
+	taskController := controller.NewTaskController(taskUsecase)
+	e := router.NewRouter(taskController)
+	e.Logger.Fatal(e.Start(":8080"))
 }
